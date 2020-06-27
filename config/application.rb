216f301c576b16a7f85175957edc343aada9dc16
bookpro <smapira@routeflags.com>
2020-06-28 00:26:32 +0900

    :truck: Change application name

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Rails6VueAlpine
+module StarterRails6Vue
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
