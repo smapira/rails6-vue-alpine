dc02d327387cdecccde92b66dec3311010b43c84
bookpro <smapira@routeflags.com>
2020-06-30 15:16:12 +0900

    :wrench: Implement typescript compiler for vue

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -3,3 +3,16 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
+
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const path = require("path");
+
+environment.plugins.append(
+  "ForkTsCheckerWebpackPlugin",
+  new ForkTsCheckerWebpackPlugin({
+    typescript: {
+      tsconfig: path.resolve(__dirname, "../../tsconfig.json"),
+    },
+    async: false,
+  })
+);
